ff01d628986762ab31f94ae813c05fd1f8e1b777
Johan Janérs <johanjaners@gmail.com>
2026-04-03 10:30:56 +0200

    feat: load recipes from JSON and configure InMemoryRecipeRepository

--- src/RecipeSearch.Api/Program.cs
+++ src/RecipeSearch.Api/Program.cs
@@ -1,12 +1,19 @@
+using RecipeSearch.Application.Interfaces;
+using RecipeSearch.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var loader = new JsonRecipeLoader();
+var dataPath = Path.Combine(builder.Environment.ContentRootPath, "data", "20170107-061401-recipeitems.json");
+var recipes = await loader.LoadAsync(dataPath);
+
+builder.Services.AddSingleton<IRecipeRepository>(
+    new InMemoryRecipeRepository(recipes));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +21,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
