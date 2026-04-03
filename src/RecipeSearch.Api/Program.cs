2f5c30d6a40887a57ac0adf41009c25a95cb67f9
Johan Janérs <johanjaners@gmail.com>
2026-04-03 15:54:51 +0200

    feat: update Program.cs to improve service registration and data loading

--- src/RecipeSearch.Api/Program.cs
+++ src/RecipeSearch.Api/Program.cs
@@ -1,16 +1,23 @@
 using RecipeSearch.Application.Interfaces;
+using RecipeSearch.Application.Services;
 using RecipeSearch.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
 var loader = new JsonRecipeLoader();
-var dataPath = Path.Combine(builder.Environment.ContentRootPath, "data", "20170107-061401-recipeitems.json");
-var recipes = await loader.LoadAsync(dataPath);
+var dataPath = Path.Combine(builder.Environment.ContentRootPath, "..", "..", "data", "20170107-061401-recipeitems.json");
+var fullDataPath = Path.GetFullPath(dataPath);
+var recipes = await loader.LoadAsync(fullDataPath);
 
 builder.Services.AddSingleton<IRecipeRepository>(
     new InMemoryRecipeRepository(recipes));
 
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<IQueryInterpretationService, SimpleQueryInterpretationService>();
+builder.Services.AddScoped<IRecipeRankingService, RecipeRankingService>();
+builder.Services.AddScoped<IRecipeSearchService, RecipeSearchService>();
 
 var app = builder.Build();
 
@@ -20,5 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
